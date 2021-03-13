f0dc8e5e8fbc7f0763c3c7ec0b9087ffe1feb438
Bistou1 <62120599+Bistou1@users.noreply.github.com>
2021-03-13 02:26:28 -0500

    First commit version 1.10
    Adding the scripts of a version said to be 1.10 before adding 1.11 on top and seeing the changes

---
+++ FX/ItemSelectedFX.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace SurvivalEngine
+{
+    /// <summary>
+    /// FX that shows an item following the mouse when selected
+    /// </summary>
+
+    public class ItemSelectedFX : MonoBehaviour
+    {
+        public GameObject icon_group;
+        public SpriteRenderer icon;
+        public Text title;
+
+        private static ItemSelectedFX _instance;
+
+        void Awake()
+        {
+            _instance = this;
+            icon_group.SetActive(false);
+            title.enabled = false;
+        }
+
+        void Update()
+        {
+            transform.position = PlayerControlsMouse.Get().GetPointingPos();
+            transform.rotation = Quaternion.LookRotation(TheCamera.Get().transform.forward, Vector3.up);
+
+            ItemSlot slot = ItemSlotPanel.GetSelectedSlotInAllPanels();
+
+            Selectable select = Selectable.GetNearestHover(transform.position);
+            PlayerCharacter player = PlayerCharacter.GetFirst();
+            MAction maction = slot != null && slot.GetItem() != null ? slot.GetItem().FindMergeAction(select) : null;
+            title.enabled = maction != null && player != null && maction.CanDoAction(player, slot, select);
+            title.text = maction != null ? maction.title : "";
+
+            bool active = slot != null && !PlayerControls.Get().IsGamePad();
+            if (active != icon_group.activeSelf)
+                icon_group.SetActive(active);
+
+            if (slot != null && slot.GetItem())
+            {
+                icon.sprite = slot.GetItem().icon;
+            }
+        }
+
+
+        public static ItemSelectedFX Get()
+        {
+            return _instance;
+        }
+    }
+
+}
---
+++ FX/ItemTakeFX.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SurvivalEngine
+{
+    /// <summary>
+    /// FX that show the item going to the inventory when picked
+    /// </summary>
+
+    public class ItemTakeFX : MonoBehaviour
+    {
+        public SpriteRenderer icon;
+        public float fx_speed = 10f;
+
+        private Vector3 start_pos;
+        private Vector3 start_scale;
+        private InventoryType inventory_target;
+        private int slot_target = -1;
+        private float timer = 0f;
+
+        private void Awake()
+        {
+            start_pos = transform.position;
+            start_scale = transform.localScale;
+        }
+
+        void Start()
+        {
+            if (slot_target < 0)
+                Destroy(gameObject);
+        }
+
+        void Update()
+        {
+            ItemSlotPanel panel = ItemSlotPanel.Get(inventory_target);
+
+            if (panel != null)
+            {
+                Vector3 wPos = panel.GetSlotWorldPosition(slot_target);
+                Vector3 dir = wPos - transform.position;
+                Vector3 tDir = wPos - start_pos;
+                float mdist = Mathf.Min(fx_speed * Time.deltaTime, dir.magnitude);
+                float scale = dir.magnitude / tDir.magnitude;
+                transform.position += dir.normalized * mdist;
+                transform.localScale = start_scale * scale;
+                transform.rotation = Quaternion.LookRotation(TheCamera.Get().transform.forward, Vector3.up);
+
+                if (dir.magnitude < 0.1f)
+                    Destroy(gameObject);
+            }
+
+            timer += Time.deltaTime;
+            if (timer > 2f)
+                Destroy(gameObject);
+        }
+
+        public void SetItem(ItemData item, InventoryType inventory, int slot)
+        {
+            inventory_target = inventory;
+            slot_target = slot;
+            icon.sprite = item.icon;
+        }
+
+        public static void DoTakeFX(Vector3 pos, ItemData item, InventoryType inventory, int target_slot)
+        {
+            if (GameData.Get().item_take_fx != null)
+            {
+                GameObject fx = Instantiate(GameData.Get().item_take_fx, pos, Quaternion.identity);
+                fx.GetComponent<ItemTakeFX>().SetItem(item, inventory, target_slot);
+            }
+        }
+    }
+
+}
