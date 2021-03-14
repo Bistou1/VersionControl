c3ca5426973f81fb851c7725435f0ff6cdcd2762
Bistou1 <62120599+Bistou1@users.noreply.github.com>
2021-03-14 19:24:37 -0400

    Base like 1.10

---
+++ Gameplay/AnimalAnim.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SurvivalEngine
+{
+
+    /// <summary>
+    /// Script that manages Animal animations
+    /// </summary>
+
+    [RequireComponent(typeof(Animal))]
+    public class AnimalAnim : MonoBehaviour
+    {
+        private Animal animal;
+        private Selectable select;
+        private Animator animator;
+        private Animator animator_outline;
+
+        void Start()
+        {
+            select = GetComponent<Selectable>();
+            animal = GetComponent<Animal>();
+            animator = GetComponentInChildren<Animator>();
+
+            GameObject outline = GetComponent<Selectable>().outline;
+            if (outline != null)
+                animator_outline = outline.GetComponentInChildren<Animator>();
+
+            animal.onAttack += OnAttack;
+            animal.onDamaged += OnDamaged;
+            animal.onDeath += OnDeath;
+
+            if (animator == null)
+                enabled = false;
+        }
+
+        void Update()
+        {
+            bool paused = TheGame.Get().IsPaused();
+            animator.enabled = !paused && select.AreScriptsActive();
+
+            if (animator.enabled)
+            {
+                animator.SetBool("Move", animal.IsMoving() && animal.IsActive());
+                animator.SetBool("Run", animal.IsRunning() && animal.IsActive());
+
+                if (animator_outline != null)
+                {
+                    animator_outline.Play(animator.GetCurrentAnimatorStateInfo(0).fullPathHash, 0, animator.GetCurrentAnimatorStateInfo(0).normalizedTime);
+                }
+            }
+        }
+
+        void OnAttack()
+        {
+            animator.SetTrigger("Attack");
+        }
+
+        void OnDamaged()
+        {
+            animator.SetTrigger("Damaged");
+        }
+
+        void OnDeath()
+        {
+            animator.SetTrigger("Death");
+        }
+    }
+
+}
---
+++ Gameplay/MixingPot.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SurvivalEngine {
+
+    public class MixingPot : MonoBehaviour
+    {
+        public ItemData[] recipes;
+        public int max_items = 6;
+        public bool clear_on_mix = false;
+
+        private Selectable select;
+
+        void Start()
+        {
+            select = GetComponent<Selectable>();
+
+            select.onUse += OnUse;
+        }
+
+        void Update()
+        {
+
+        }
+
+        private void OnUse(PlayerCharacter player)
+        {
+            MixingPanel.Get().ShowMixing(player, this, select.GetUID());
+        }
+    }
+
+}
